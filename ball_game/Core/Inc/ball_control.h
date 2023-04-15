0006066d30971ef1a9fa658f7bbbd9e7ef413363
Stanislaw Chedoska <259354@student.pwr.edu.pl>
2023-04-15 14:02:01 +0200

    Fizyka piłki

--- ball_game/Core/Inc/ball_control.h
+++ ball_game/Core/Inc/ball_control.h
@@ -9,14 +9,21 @@
 #define INC_BALL_CONTROL_H_
 
 #include "gyroscope.h"
+#include "math.h"
+
+#define PI_CONST 3.1415926
+#define GRAVITY_CONST 4
+#define FRICTION_CONST 1
 
 struct Ball_control_data
 {
 	float ctrlX_angle;
 	float ctrlY_angle;
 	float ctrlZ_angle;
-	uint16_t X_screen_pos;
-	uint16_t Y_screen_pos;
+	float X_screen_pos;
+	float Y_screen_pos;
+	float X_screen_speed;
+	float Y_screen_speed;
 } typedef Ball_control_data;
 
 void ball_ctrl_init(Ball_control_data *ball_data, uint16_t X_start_pos, uint16_t Y_start_pos);
--- ball_game/Core/Src/ball_controll.c
+++ ball_game/Core/Src/ball_controll.c
@@ -11,6 +11,8 @@ void ball_ctrl_init(Ball_control_data *ball_data, uint16_t X_start_pos, uint16_t
 	ball_ctrl_restetDrift(ball_data);
 	ball_data->X_screen_pos = X_start_pos;
 	ball_data->Y_screen_pos = Y_start_pos;
+	ball_data->X_screen_speed = 0;
+	ball_data->Y_screen_speed = 0;
 }
 
 void ball_ctrl_restetDrift(Ball_control_data *ball_data)
@@ -30,5 +32,30 @@ void ball_update_ctrl_angles(Ball_control_data *ball_data, L3GD20 *L3GD20_data, 
 
 void ball_update_pos(Ball_control_data *ball_data, int delta_time_ms)
 {
+	float delta_time = delta_time_ms / 1000.0;
+	float X_radAngle = (ball_data->ctrlX_angle * PI_CONST / 180.0);
+	float Y_radAngle = (ball_data->ctrlY_angle * PI_CONST / 180.0);
 
+	ball_data->X_screen_speed += sinf(X_radAngle) * GRAVITY_CONST * delta_time;
+	ball_data->Y_screen_speed += sinf(Y_radAngle) * GRAVITY_CONST * delta_time;
+
+	if(ball_data->X_screen_speed > 0)
+		ball_data->X_screen_speed -= FRICTION_CONST * delta_time;
+	else
+		ball_data->X_screen_speed += FRICTION_CONST * delta_time;
+
+	if(ball_data->Y_screen_speed > 0)
+			ball_data->Y_screen_speed -= FRICTION_CONST * delta_time;
+		else
+			ball_data->Y_screen_speed += FRICTION_CONST * delta_time;
+
+	ball_data->X_screen_pos += (ball_data->X_screen_speed * delta_time);
+	ball_data->Y_screen_pos += (ball_data->Y_screen_speed * delta_time);
 }
+
+
+
+
+
+
+
